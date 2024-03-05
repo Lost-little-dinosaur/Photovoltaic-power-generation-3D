f7fc5f7f4aca44df7827a246ba606cea141cee7b
wjy <1471947151@qq.com>
2024-03-05 22:07:34 +0800

    roof:7

--- classes/obstacle.py
+++ classes/obstacle.py
@@ -7,6 +7,7 @@ class Obstacle:
         UNIT = getUnit()
         self.type = obstacle["type"]
         self.ID = obstacle["id"]
+
         if obstacle["type"] == "有烟烟囱" or obstacle["type"] == "无烟烟囱":
             self.height = obstacle["height"]
             if not obstacle["isRound"]:
@@ -37,6 +38,12 @@ class Obstacle:
                 self.diameter = obstacle["diameter"] / UNIT
                 self.centerPosition = [obstacle["centerPosition"][0] / UNIT, obstacle["centerPosition"][1] / UNIT]
                 # todo: 圆形的烟囱暂时不做计算阴影
+        elif obstacle["type"] == "屋面扣除":
+            self.height = obstacle["height"]
+            self.isRound = False
+            self.width = obstacle["width"]
+            self.length = obstacle["length"]
+            self.upLeftPosition = [obstacle["upLeftPosition"][0], obstacle["upLeftPosition"][1]]
         elif obstacle["type"] == "热水器":  # 暂时只支持烟囱输入
             pass  # todo
         elif obstacle["type"] == "水塔":
