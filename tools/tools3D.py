1753d90ee6c453e2a2f0ac14feabb9e4cca3ee15
otoribird <otori_bird@zju.edu.cn>
2024-05-10 14:13:45 +0800

    fix irregular roof shape bug

--- tools/tools3D.py
+++ tools/tools3D.py
@@ -8,14 +8,29 @@ from typing import List
 from copy import deepcopy
 import multiprocessing
 
-def multiprocess_func(func, iter):
-    processes = multiprocessing.cpu_count()
-    # pool = multiprocessing.Pool(processes=processes)
-    with multiprocessing.Pool(processes=processes) as pool:
-        results = pool.map(func=func,iterable=iter)
-    # pool.close()
-    # pool.join()
+def multiprocess_func(func, args_list, timeout=600):
+    pool = multiprocessing.Pool()
+    results = []
+    for args in args_list:
+        result = pool.apply_async(func, args)
+        try:
+            result = result.get(timeout=timeout)  # 设置超时时间
+            results.append(result)
+        except multiprocessing.TimeoutError:
+            # 如果超时，抛出异常
+            raise TimeoutError("Multiprocessing function execution timed out")
+    pool.close()
+    pool.join()
     return results
+
+# def multiprocess_func(func, iter):
+#     processes = multiprocessing.cpu_count()
+#     # pool = multiprocessing.Pool(processes=processes)
+#     with multiprocessing.Pool(processes=processes) as pool:
+#         results = pool.map(func=func,iterable=iter)
+#     # pool.close()
+#     # pool.join()
+#     return results
 
 
 def draw3dModel(model3DArray):
@@ -430,7 +445,6 @@ def check_point_in_polygon(args):
     return point_in_polygon(point, polygon)
 
 def create_bounding_box_with_holes(polygon):
-    # 获取多边形的边界框
     min_x = min(polygon, key=lambda p: p[0])[0]
     max_x = max(polygon, key=lambda p: p[0])[0]
     min_y = min(polygon, key=lambda p: p[1])[1]
@@ -439,15 +453,39 @@ def create_bounding_box_with_holes(polygon):
     bounding_box = np.zeros((max_y - min_y + 1, max_x - min_x + 1))
     points_to_check = [(x, y) for y in range(min_y, max_y + 1) for x in range(min_x, max_x + 1)]
     
-    # 使用多进程池进行并行计算
-    results = multiprocess_func(check_point_in_polygon, [(point, polygon) for point in points_to_check])
-
+    try:
+        results = multiprocess_func(check_point_in_polygon, [(point, polygon) for point in points_to_check])
+    except TimeoutError as e:
+        # 处理超时异常
+        print("Error:", e)
+        raise
+    
     for idx, inside in enumerate(results):
         y, x = divmod(idx, max_x - min_x + 1)
         if not inside:
             bounding_box[y, x] = INF
     
     return bounding_box
+
+# def create_bounding_box_with_holes(polygon):
+#     # 获取多边形的边界框
+#     min_x = min(polygon, key=lambda p: p[0])[0]
+#     max_x = max(polygon, key=lambda p: p[0])[0]
+#     min_y = min(polygon, key=lambda p: p[1])[1]
+#     max_y = max(polygon, key=lambda p: p[1])[1]
+    
+#     bounding_box = np.zeros((max_y - min_y + 1, max_x - min_x + 1))
+#     points_to_check = [(x, y) for y in range(min_y, max_y + 1) for x in range(min_x, max_x + 1)]
+    
+#     # 使用多进程池进行并行计算
+#     results = multiprocess_func(check_point_in_polygon, [(point, polygon) for point in points_to_check])
+
+#     for idx, inside in enumerate(results):
+#         y, x = divmod(idx, max_x - min_x + 1)
+#         if not inside:
+#             bounding_box[y, x] = INF
+    
+#     return bounding_box
 
 
 def get_polygon_area(vertices):
